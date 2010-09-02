b20e23c8c1306fc663bfd915c9272afdd57253c4
Mitchell Amihod <mitchell.amihod@gmail.com>
2010-09-02 11:33:32 -0400

    Move away from use of $ More commenting, moving things around so file reads a bit more linearly.

--- assets/ajaxify-shop.js
+++ assets/ajaxify-shop.js
@@ -1,12 +1,13 @@
 /**
- * Shopify Add To Cart. 
+ * Shopify Ajaxify Shop. 
  * 
- * @uses Shopify jQuery API http://mayert-douglas4935.myshopify.com/shopify/api.jquery.js.
- *          We make some mods. consider them feedback :) See comments
+ * @uses Modified Shopify jQuery API (link to it)
  *
  */
-$(document).ready(function() { 
+jQuery(document).ready(function() { 
 //Begin Wrapper
+
+var jQ = jQuery;
 
 var selectors = {
     TOTAL_ITEMS: '.cart-total-items'
@@ -17,13 +18,34 @@ var text = {
     ITEMS: 'Items'
 };
 
-//When an item is added to the cart, we re-enable the button.
-//This is where you would want to put any flash messaging, for example.
-//@param object line_item
-//@param HTMLelement/String form string selector, or HTMLelement itself. 
+//Attach Submit Handler to all forms with the /cart/add action. 
+jQ('form[action=/cart/add]').submit(function(e) {
+    e.preventDefault();
+    //Can't use updateCartFromForm since you need the item added before you can update (otherwise, would have been more convenient)
+    //So, in onItemAdded, we Shopify.getCart() to force the repaint of items in cart. 
+
+    //Disable the Add To Cart button, add a disabled class. 
+    jQ(e.target).find('input[type=image]').attr('disabled', true).addClass('disabled');
+    Shopify.addItemFromForm(e.target);
+});
+
+/**
+ * Shopify.onItemAdded
+ * 
+ * Triggered by the response when something is added to the cart via the add to cart button.
+ * This is where you would want to put any flash messaging, for example.
+ * 
+ * @param object line_item
+ * @param HTMLelement/String Form HTMLElement, or selector
+ */
 Shopify.onItemAdded = function(line_item, form) {
-    $(form).find('input[type=image]').attr('disabled', false).removeClass('disabled');
+    //Default behaviour for this modification:
+    //When a Add To Cart form is clicked, we disable the button and apply a class of disabled. 
+    //Here is where we remove the disabled class, and reactivate the button.
+    jQ(form).find('input[type=image]').attr('disabled', false).removeClass('disabled');
+
     //You can add any extra messaging you would want here. 
+
     //Get the state of the cart, which will trigger onCartUpdate
     Shopify.getCart();
 };
@@ -33,7 +55,7 @@ Shopify.onItemAdded = function(line_item, form) {
 Shopify.onCartUpdate = function(cart) {    
     var message = '<span class="count">'+cart.item_count+'</span> ' +
                     ((cart.item_count == 1) ? text.ITEM : text.ITEMS );
-    $(selectors.TOTAL_ITEMS).html(message);
+    jQ(selectors.TOTAL_ITEMS).html(message);
 };
 
 //What to display when there is an error. You tell me?! I've left in a commented out example.
@@ -44,13 +66,6 @@ Shopify.onError = function(XMLHttpRequest, textStatus) {
   // var data = eval('(' + XMLHttpRequest.responseText + ')');
   // errorMessage(data.message + '(' + data.status  + '): ' + data.description);
 };
-
-$('form[action=/cart/add]').submit(function(e) {
-    e.preventDefault();
-    //Can't use updateCartFromForm since you need the item added before you can update, so we just get cart to repaint.
-    $(e.target).find('input[type=image]').attr('disabled', true).addClass('disabled');
-    Shopify.addItemFromForm(e.target);
-});
 
 var testErrorMessage = function() {
     var errorTest = {
